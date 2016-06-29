95a893a184cfb346928abf3d2092c1111c90442e
choizhang <461261438@qq.com>
2016-06-29 13:29:41 +0800

    实现了重复表的可拖动

--- src/addCustomizeDialog.js
+++ src/addCustomizeDialog.js
@@ -37,7 +37,7 @@ UE.registerUI('dialog',function(editor,uiName){
                     }
 
                     //var html = '<table class="component" draggable="true" ondragstart="event.dataTransfer.setData(\'text/plain\', \'This text may be dragged\'); ">' ;
-                    var html = '<table class="component" draggable="true">' ;
+                    var html = '<table class="component" draggable="false">' ;
                     html += '<tr><th>11<button class="component-handle">v</button></th><th>22</th><th>33</th></tr><tr>'
                     for(var i=0; i<column; i++){
                         html += '<td><img src="text.png" id="editorComp_' + (100 + newCount) + '" title="title" alt="文本域" style="width: 55px;height: 30px;text-align: center;line-height: 30px;font-size: 17px;"></td>';
