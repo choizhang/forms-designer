f926fecc463c5e7123ff0a02bae82600e0fed96c
choizhang <461261438@qq.com>
2016-07-28 10:48:28 +0800

    罗宾布局修改完成

--- src/ueditor.all.js
+++ src/ueditor.all.js
@@ -29339,9 +29339,16 @@ UE.version = "1.4.3";
                                         if(index === 1){
                                             //如果是第一个就添加html
                                             var str = toolbarItemUi.getHtmlTpl();
-                                            toolbarItemUi.getHtmlTpl = function() {
-                                                return '<div class="mod"><div>'+ toolbarItem[0] +'</div>' + str;
-                                            }
+                                            //toolbarItemUi.getHtmlTpl = function() {
+                                            //    return '<div class="mod"><div>'+ toolbarItem[0] +'</div>' + str;
+                                            //}
+
+                                            toolbarItemUi.getHtmlTpl = (function(name) {
+                                                return function() {
+                                                    return '<div class="mod"><div class="mod-title">'+ name +'</div>' + str;
+                                                }
+                                            })(toolbarItem[0])
+
                                         }
                                         //
                                         if(index === toolbarItem.length - 1){
