c1cfa5c0e781ef6075b04e290f4981d5b7bfb503
choizhang <461261438@qq.com>
2016-08-09 15:03:06 +0800

    选中单元格背景色获取错误的bugfix

--- src/iframe.js
+++ src/iframe.js
@@ -219,14 +219,6 @@ $(function () {
             });
     }
 
-    function rgb2hex(rgb) {
-        rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-        function hex(x) {
-            return ("0" + parseInt(x).toString(16)).slice(-2);
-        }
-        return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
-    }
-
     window.iframe = {
         bindIframe: bindIframe
     }
